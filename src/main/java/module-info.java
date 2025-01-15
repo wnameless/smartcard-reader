e792dd8e5990af2ec8a759dd262c9563193ce7e4
wmw <wnameless@gmail.com>
2025-01-15 19:56:53 +0800

    Improve module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.github.wnameless.smartcard {
   requires com.google.common;
   requires java.desktop;
   requires java.logging;
-  requires java.smartcardio;
+  requires transitive java.smartcardio;
   requires net.sf.rubycollect4j;
+
+  exports com.github.wnameless.smartcard;
 }
